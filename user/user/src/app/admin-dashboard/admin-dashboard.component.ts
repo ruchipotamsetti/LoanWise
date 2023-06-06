d2164705d75f6ed949fb23be00a66ed1b836888c
Piyush Thangan <P7113627@ntil.com>
2023-06-06 10:07:46 +0530

    'piyush2'

--- user/user/src/app/admin-dashboard/admin-dashboard.component.ts
+++ user/user/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -6,7 +6,7 @@ import { LoanApplications } from '../loanapplications.model';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+  styleUrls: ['./admin-dashboard.component .css']
 })
 export class AdminDashboardComponent {
   constructor(private _router:Router, private _adminService: AdminDashboardService){}
