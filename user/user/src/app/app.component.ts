ec80e81999966da1d6cc3b0afed43a3c8a927ba7
vinitjindam75 <vinitjindam75@gmail.com>
2023-06-08 13:09:39 +0530

    "vinitLogOutNav0707"

--- user/user/src/app/app.component.ts
+++ user/user/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { SharedHeaderFooterService } from './shared-header-footer.service';
+import { NavigationEnd, Route, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,32 @@ export class AppComponent {
   showHeaderAndFooter= true;
 
  sharedService:SharedHeaderFooterService;
-constructor(shared :SharedHeaderFooterService){
+constructor(shared :SharedHeaderFooterService, private _router:Router){
   this.sharedService=shared;
   this.sharedService.parentProperty = true; // Set the initial value
 
 }
+  pageReload: boolean = false;
+
+isLoggedIn(): boolean {
+  // Retrieve the email from local storage
+  const email = localStorage.getItem('email');
+  
+  // Return true if email is present, indicating the user is logged in
+  return email !== null;
+}
+logout(): void {
+  // Remove the email from local storage to log out the user
+  localStorage.clear();
+  this.reload();
+
+}
+reload() {
+  if (!this.pageReload) {
+    this.pageReload = true;
+    window.location.reload();
+  }
+}
+
 
 }
